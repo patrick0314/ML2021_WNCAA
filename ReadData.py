9dba3ee3f7d21a163989e67fba605faaba9bf8d4
patrick0314 <47914151+patrick0314@users.noreply.github.com>
2021-03-19 11:43:11 +0800

    Update ReadData.py

--- ReadData.py
+++ ReadData.py
@@ -1,78 +1,82 @@
 import pandas as pd
 
-prefix = 'Data/'
+#prefix = 'Data/WDataFiles_Stage1'
+#prefix = 'Data/WDataFiles_Stage2'
 
-def City():
+def City(prefix):
     City = pd.read_csv(prefix + 'Cities.csv')
     #print(City, end='\n\n') # [453, 3] = [CityID, City, State]
     return City
 
-def Conference():
+def Conference(prefix):
     Conference = pd.read_csv(prefix + 'Conferences.csv')
     #print(Conference, end='\n\n') # [51, 2] = [ConfAbbrev, Description]
     return Conference
 
-def GameCity():
+def GameCity(prefix):
     GameCity = pd.read_csv(prefix + 'WGameCities.csv')
     #print(GameCity, end='\n\n') # [57316, 6] = [Season, DayNum, WTeamId, LTeamID, CRType, CityID]
     return GameCity
 
-def CompactResult():
+def CompactResult(prefix):
     CompactResult = pd.read_csv(prefix + 'WNCAATourneyCompactResults.csv')
     #print(CompactResult, end='\n\n') # [1386, 8] = [Season, DayNum, WTeamID, LTeamID, LScore, WLoc, NumOT]
     return CompactResult
 
-def DetailedResult():
+def DetailedResult(prefix):
     DetailedResult = pd.read_csv(prefix + 'WNCAATourneyDetailedResults.csv')
     #print(DetailedResult, end='\n\n') # [630, 34] = [Season, DayNum, WTeamID, WScore, LTeamID, LScore, WLoc ... LOR, LDR, LAst, LTO, LSTl, LBlk, LPF]
     return DetailedResult
 
-def Seed():
+def Seed(prefix):
     Seed = pd.read_csv(prefix + 'WNCAATourneySeeds.csv')
     #print(Seed, end='\n\n') # [1408, 3] = [Season, Seed, TeamID]
     return Seed
 
-def Slot():
+def Slot(prefix):
     Slot = pd.read_csv(prefix + 'WNCAATourneySlots.csv')
     #print(Slot, end='\n\n') # [63, 3] = [Slot, StrongSeed, WeakSeed]
     return Slot
 
-def RegularCompactResult():
+def RegularCompactResult(prefix):
     RegularCompactResult = pd.read_csv(prefix + 'WRegularSeasonCompactResults.csv')
     #print(RegularCompactResult, end='\n\n') # [112183, 8] = [Season, DayNum, WTeamID, WScore, LTeamID, LScore, WLoc, NumOT]
     return RegularCompactResult
 
-def RegularDetailedResult():
+def RegularDetailedResult(prefix):
     RegularDetailedResult = pd.read_csv(prefix + 'WRegularSeasonDetailedResults.csv')
     #print(RegularDetailedResult, end='\n\n') # [56793, 34] = [Season, DayNum, WTeamID, WScore, LTeamID, LScore, WLoc ... LOR, LDR, LAst, LTO, LSTl, LBlk, LPF]
     return RegularDetailedResult
 
-def Season():
+def Season(prefix):
     Season = pd.read_csv(prefix + 'WSeasons.csv')
     #print(Season, end='\n\n') # [24, 6] = [Season, DayZero, RegionW, RegionX, RegionY, RegionZ]
     return Season
 
-def TeamConference():
+def TeamConference(prefix):
     TeamConference  = pd.read_csv(prefix + 'WTeamConferences.csv')
     #print(TeamConference, end='\n\n') # [8051, 3] = [Season, TeamID, ConfAbbrev]
     return TeamConference
 
-def Team():
+def Team(prefix):
     Team = pd.read_csv(prefix + 'WTeams.csv')
     #print(Team, end='\n\n') # [369, 2] = [TeamID, TeamName]
     return Team
 
-def TeamSpelling():
+def TeamSpelling(prefix):
     TeamSpelling = pd.read_csv(prefix + 'WTeamSpellings.csv', encoding='cp1252')
     #print(TeamSpelling, end='\n\n') # [1140, 2] = [TeamNameSpelling, TeamID]
     return TeamSpelling
 
-def Output():
-    Output = pd.read_csv(prefix + 'WSampleSubmissionStage1.csv')
+def Output(prefix):
+    if prefix[-2] == '1':
+        Output = pd.read_csv(prefix + 'WSampleSubmissionStage1.csv')
+    if prefix[-2] == '2':
+        Output = pd.read_csv(prefix + 'WSampleSubmissionStage2.csv')
     #print(Output, end='\n\n')
     return Output
 
-def Rating():
+def Rating(prefix):
     rating = pd.read_csv(prefix + '538ratingsWomen.csv')
     #print(rating, end='\n\n')
     rate = []
